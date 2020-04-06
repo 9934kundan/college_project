20f9883e264e5e8d05d6e260316f97803b9a4999
9934kundan <50224046+9934kundan@users.noreply.github.com>
2020-04-06 12:30:30 +0530

    Create services.go

---
+++ services.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
+
+// Main
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting the chaincode: %s", err)
+	}
+}
+
+// Init initializes chaincode
+
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+// Invoke - Our entry point for Invocations
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	function, args := stub.GetFunctionAndParameters()
+	fmt.Println("invoke is running " + function)
+
+	// Handle different functions
+	if function == "addManufacturer" { //create a new user
+		return t.addmanufacturer(stub, args)
+	} else if function == "addUser" { //read a user
+		return t.adduser(stub, args)
+	} else if function == "addInsurer" { //initialize a course for a user
+		return t.addinsurer(stub, args)
+	} else if function =="addDealer {
+    return t.addDealer(stub, args)
+  }
+  
+  fmt.Println("invoke did not find function: " + function) //error
+	return shim.Error("Received unknown function call")
+  
+  }
