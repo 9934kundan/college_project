11b027f8121f62015f84d61bd5049d89339715d7
9934kundan <50224046+9934kundan@users.noreply.github.com>
2020-04-11 09:28:38 +0530

    Create read_ledger.go

---
+++ read_ledger.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+// get the car_info
+
+func (t *SimpleChaincode) car_info(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+
+	var err error
+	queryString := fmt.Sprintf("{\"selector\":{\"docType\":\"car_info\"}}")
+	queryResults, err := getQueryResultForQueryString(stub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(queryResults)
+}
+
+// get car manufacturer info
+
+func (t *SimpleChaincode) readUser(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var err error
+	queryString := fmt.Sprintf("{\"selector\":{\"docType\":\"car_info_manufacturer\"}}")
+	queryResults, err := getQueryResultForQueryString(stub, queryString)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(queryResults)
+
+	return shim.Success(valAsbytes)
+}
+
