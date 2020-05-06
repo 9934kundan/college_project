feaa53f867f0309ab43608a0d575ae7ec51894da
9934kundan <50224046+9934kundan@users.noreply.github.com>
2020-05-06 20:03:32 +0530

    Update services.go

--- services.go
+++ services.go
@@ -45,17 +45,17 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
         } else if function == "car_manufacturer_info"
            return t.car_manufacturer_info(stub, args)
         }else if function == "addCartoLedger"
-           return t.car_manufacturer_info(stub, args)
+           return t.addCartoLedger(stub, args)
         }else if function == "transferToDealer"
-           return t.car_manufacturer_info(stub, args)
+           return t.transferToDealer(stub, args)
         }else if function == "sellToCustomer"
-           return t.car_manufacturer_info(stub, args)
+           return t.sellToCustomer(stub, args)
         }else if function == "sellCar"
-           return t.car_manufacturer_info(stub, args)
+           return t.sellCar(stub, args)
         }else if function == "insuranceCar"
-           return t.car_manufacturer_info(stub, args)
+           return t.insuranceCar(stub, args)
         }else if function == "rtaApprove"
-           return t.car_manufacturer_info(stub, args)
+           return t.rtaApprove(stub, args)
         }
    fmt.Println("invoke did not find function: " + function) //error
 	return shim.Error("Received unknown function call")
